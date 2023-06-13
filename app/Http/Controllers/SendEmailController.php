6cd28d2bdab016f4bf1206b7fa9ead8ed12b04a1
ArasAiman <johnred631@gmail.com>
2023-06-14 02:11:06 +0800

    Update SendEmailController.php

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -9,38 +9,38 @@ use Illuminate\Support\Facades\DB;
 class SendEmailController extends Controller
 {
     public function send(Request $request)
-    {
-        $name = $request->input('name');
-        $fromEmail = $request->input('fromemail');
-        $toEmails = $request->input('to_email');
-        $subject = $request->input('subject');
-        $message = $request->input('message');
-        $attachment = $request->file('attachment');
+{
+    $name = $request->input('name');
+    $fromEmail = $request->input('fromemail');
+    $toEmails = $request->input('to_email');
+    $subject = $request->input('subject');
+    $message = $request->input('message');
+    $attachment = $request->file('attachment');
 
-        if (in_array('all', $toEmails)) {
-            // Fetch all email addresses from the database
-            $toEmails = DB::table('customers')->pluck('email')->toArray();
-        }
-
-        foreach ($toEmails as $toEmail) {
-            // Send email to each recipient
-            Mail::send([], [], function ($mailMessage) use ($name, $fromEmail, $toEmail, $subject, $message, $attachment) {
-                $mailMessage->to($toEmail)
-                    ->subject($subject)
-                    ->setBody($message);
-
-                if ($attachment) {
-                    $mailMessage->attach($attachment->getRealPath(), [
-                        'as' => $attachment->getClientOriginalName(),
-                        'mime' => $attachment->getMimeType(),
-                    ]);
-                }
-
-                $mailMessage->from($fromEmail, $name);
-            });
-        }
-
-        return redirect()->back()->with('success', 'Emails have been sent.');
+    // Check if "Select All" option is selected
+    if (in_array('Select All', $toEmails)) {
+        // Fetch all email addresses from the database
+        $toEmails = DB::table('customers')->pluck('email')->toArray();
     }
 
+    foreach ($toEmails as $toEmail) {
+        // Send email to each recipient
+        Mail::send([], [], function ($mailMessage) use ($name, $fromEmail, $toEmail, $subject, $message, $attachment) {
+            $mailMessage->to($toEmail)
+                ->subject($subject)
+                ->setBody($message);
+
+            if ($attachment) {
+                $mailMessage->attach($attachment->getRealPath(), [
+                    'as' => $attachment->getClientOriginalName(),
+                    'mime' => $attachment->getMimeType(),
+                ]);
+            }
+
+            $mailMessage->from($fromEmail, $name);
+        });
+    }
+
+    return redirect()->back()->with('success', 'Emails have been sent.');
+}
 }
