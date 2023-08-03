b966a73b04c06f2fac4d2e7bafae8e22547fa62a
ArasAiman <johnred631@gmail.com>
2023-08-03 21:42:07 +0800

    Update SendEmailController.php

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -1,5 +1,4 @@
 <?php
-
 // app/Http/Controllers/SendEmailController.php
 
 namespace App\Http\Controllers;
@@ -8,34 +7,50 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
 use App\Jobs\SendEmailJob;
+use App\Models\EmailTemplate; // Make sure to import the EmailTemplate model
 
 class SendEmailController extends Controller
 {
     public function send(Request $request)
-{
-    $name = $request->input('name');
-    $fromEmail = $request->input('fromemail');
-    $toEmails = $request->input('to_email');
-    $subject = $request->input('subject');
-    $message = $request->input('message');
-    $attachment = $request->file('attachment');
+    {
+        // Your existing code to retrieve form inputs
+        $name = $request->input('name');
+        $fromEmail = $request->input('fromemail');
+        $toEmails = $request->input('to_email');
+        $subject = $request->input('subject');
+        $attachment = $request->file('attachment');
 
-    // Check if "Select All" option is selected
-    if (in_array('Select All', $toEmails)) {
-        // Fetch all email addresses from the database
-        $toEmails = DB::table('customers')->pluck('email')->toArray();
-    }
+        // Fetch the selected template from the database based on the template ID
+        $templateId = $request->input('template');
+        $template = EmailTemplate::find($templateId);
 
-    foreach ($toEmails as $toEmail) {
-        // Move the uploaded file to the storage directory
-        $attachmentPath = $attachment ? $attachment->storeAs('attachments', $attachment->getClientOriginalName()) : null;
+        if (!$template) {
+            return redirect()->back()->with('error', 'Selected template not found.');
+        }
+
+        // Check if "Select All" option is selected
+        if (in_array('Select All', $toEmails)) {
+            // Fetch all email addresses and names from the database
+            $recipientData = DB::table('customers')->pluck('name', 'email');
+        } else {
+            // Fetch names of selected recipients from the database based on the email addresses
+            $recipientData = DB::table('customers')->whereIn('email', $toEmails)->pluck('name', 'email');
+        }
 
         // Send email to each recipient using the queue
-        SendEmailJob::dispatch($name, $fromEmail, $toEmail, $subject, $message, $attachmentPath);
+        foreach ($recipientData as $email => $recipientName) {
+            // Replace placeholders in the template content with actual values
+            $message = $template->content;
+            $personalizedMessage = str_replace('[RecipientName]', $recipientName, $message);
+            $personalizedMessage = str_replace('[SenderName]', $name, $personalizedMessage);
+
+            // Move the uploaded file to the storage directory
+            $attachmentPath = $attachment ? $attachment->storeAs('attachments', $attachment->getClientOriginalName()) : null;
+
+            // Send email to each recipient using the queue
+            SendEmailJob::dispatch($name, $fromEmail, $email, $subject, $personalizedMessage, $attachmentPath);
+        }
+
+        return redirect()->back()->with('success', 'Emails have been sent.');
     }
-
-    return redirect()->back()->with('success', 'Emails have been sent.');
-}
-
-
 }
