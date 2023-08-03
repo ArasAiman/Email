88ad6fa4735f79cce7cd55cf8e37168621a51121
ArasAiman <johnred631@gmail.com>
2023-08-03 18:28:03 +0800

    Update template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,49 +3,50 @@
 @section('content')
     <div class="col-xl">
         <div class="card mb-4">
-          <div class="card-body">
-            <script src="https://cdn.ckeditor.com/ckeditor5/39.0.0/classic/ckeditor.js"></script>
-            <form method="post" action="{{ route('email_template.store') }}">
-                @csrf
-            <div class="mb-3">
-                <label class="form-label" for="basic-icon-default-company">Template Name </label>
-                <div class="input-group input-group-merge">
-                  <span id="basic-icon-default-company2" class="input-group-text"
-                    ><i class='bx bx-message-dots' ></i>
-                  </span>
-                  <input
-                    type="text"
-                    name="templatename" id="templatename"
-                    class="form-control"
-                    placeholder="Template Name"
-                    aria-label="ACME Inc."
-                    aria-describedby="basic-icon-default-company2"
-                  />
-                </div>
-              </div>
-            <div>
-                <textarea name="email_template" id="email_template"></textarea>
+            <div class="card-body">
+                <script src="https://cdn.ckeditor.com/ckeditor5/39.0.0/classic/ckeditor.js"></script>
+                <form method="post" action="{{ route('email_template.store') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label" for="basic-icon-default-company">Template Name </label>
+                        <div class="input-group input-group-merge">
+                            <span id="basic-icon-default-company2" class="input-group-text"
+                                ><i class='bx bx-message-dots' ></i>
+                            </span>
+                            <input
+                                type="text"
+                                name="templatename" id="templatename"
+                                class="form-control"
+                                placeholder="Template Name"
+                                aria-label="ACME Inc."
+                                aria-describedby="basic-icon-default-company2"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <textarea name="email_template" id="email_template"></textarea>
 
-                <script>
-                    ClassicEditor
-                        .create(document.querySelector('#email_template'))
-                        .then(editor => {
-                            console.log('Editor was initialized', editor);
-                        })
-                        .catch(error => {
-                            console.error('Error initializing editor:', error);
-                        });
-                </script>
+                        <script>
+                            ClassicEditor
+                                .create(document.querySelector('#email_template'))
+                                .then(editor => {
+                                    console.log('Editor was initialized', editor);
+                                })
+                                .catch(error => {
+                                    console.error('Error initializing editor:', error);
+                                });
+                        </script>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#emailSentModal">Save Template</button>    <br>
+                    </div>
+                </form>
             </div>
-          </div>
         </div>
-      </div>
-  <div>
-    <button type="submit" class="btn btn-primary" data-bs-toggle="modal"
-    data-bs-target="#emailSentModal">Save Template</button>    <br>
-</div>
-</form>
-@if(session('success'))
+    </div>
+
+    @if(session('success'))
         <div class="alert alert-success">
             {{ session('success') }}
         </div>
@@ -69,7 +70,7 @@
                                     <div class="card">
                                         <div class="card-body">
                                             <h5 class="card-title">{{ $template->name }}</h5>
-                                            <p class="card-text">{{ $template->content }}</p>
+                                            <p class="card-text">{!! strip_tags($template->content) !!}</p>
                                         </div>
                                     </div>
                                 </div>
