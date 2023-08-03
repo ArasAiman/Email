921988247caec072f8577215b9f218836e70f99c
ArasAiman <johnred631@gmail.com>
2023-08-03 14:46:02 +0800

    Update template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,6 +5,22 @@
         <div class="card mb-4">
           <div class="card-body">
             <script src="https://cdn.ckeditor.com/ckeditor5/39.0.0/classic/ckeditor.js"></script>
+            <div class="mb-3">
+                <label class="form-label" for="basic-icon-default-company">Template Name </label>
+                <div class="input-group input-group-merge">
+                  <span id="basic-icon-default-company2" class="input-group-text"
+                    ><i class='bx bx-message-dots' ></i>
+                  </span>
+                  <input
+                    type="text"
+                    name="templatename" id="templatename"
+                    class="form-control"
+                    placeholder="Template Name"
+                    aria-label="ACME Inc."
+                    aria-describedby="basic-icon-default-company2"
+                  />
+                </div>
+              </div>
             <div>
                 <textarea name="email_template" id="email_template"></textarea>
 
